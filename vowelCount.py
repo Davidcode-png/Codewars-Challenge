0a819c05b26e5d1ec0a6dd62b4bcfee5ad053e6b
Ojewale Opeoluwa <74027215+Davidcode-png@users.noreply.github.com>
2022-06-16 23:21:55 +0100

    Delete vowelCount.py

--- vowelCount.py
+++
@@ -1,9 +0,0 @@
-def get_count(sentence):
-    count = 0
-    vowels  = 'aeiou'
-    for i in sentence:
-        if i in vowels:
-            count += 1
-    return count
-
-print(get_count('David'))
